e10f0cd7820f6a3e4672a45aa594bbb826b7273a
Pasquii <pascualpau04@gmail.com>
2026-01-12 11:33:28 +0100

    feat: add components index

---
+++ frontend/src/js/components/index.js
@@ -0,0 +1,28 @@
+/**
+ * Components Index - Export all components
+ */
+
+import Card from './card.js';
+import Button from './button.js';
+import Badge from './badge.js';
+import Input from './input.js';
+import Select from './select.js';
+import Loader from './loader.js';
+
+export {
+  Card,
+  Button,
+  Badge,
+  Input,
+  Select,
+  Loader,
+};
+
+export default {
+  Card,
+  Button,
+  Badge,
+  Input,
+  Select,
+  Loader,
+};
